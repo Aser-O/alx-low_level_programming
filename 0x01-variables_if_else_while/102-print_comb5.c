e45fcb28ba40203fbd77283a342cd55c0c85acb6
Aser-O <Aserosama1@gmail.com>
2023-08-11 17:55:38 -0700

    99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+	int j = 0;
+	int k = 0;
+	int l = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = j+1; k < 10; k++)
+			{
+				for (l = i+1; l < 10; l++)
+				{
+					putchar(48 + i);
+					putchar(48 + j);
+					putchar(' ');
+					putchar(48 + k);
+					putchar(48 + l);
+					if (!(k == 9 && j == 8 && i == 7))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
