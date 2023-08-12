a09e41ef9396ea82277bc0ab88357d60ac61b459
Aser-O <Aserosama1@gmail.com>
2023-08-11 17:18:29 -0700

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i = 0;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+	for (i = 0; i < 10; i++)
+		putchar(a[i] + 48);
+
+	for (i = 0; i < 6; i++)
+		putchar((int)'a' + i);
+
+	putchar('\n');
+	return (0);
+}
