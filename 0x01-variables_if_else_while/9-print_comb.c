795a95838beae3f224e16bc8b6bc77293a532906
Aser-O <Aserosama1@gmail.com>
2023-08-11 17:27:05 -0700

    999

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,12 +13,13 @@ int main(void)
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + 48);
-		if(i != 9)
-	       	{
+		if (i != 9)
+		{
 			putchar(',');
 			putchar(' ');
 		}
 	}
+
 	putchar('\n');
 	return (0);
 }
