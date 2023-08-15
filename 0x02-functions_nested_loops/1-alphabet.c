418ee9681495be9f96148e49b25e952967ee91a7
Aser-O <Aserosama1@gmail.com>
2023-08-15 11:28:53 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = (int)'a'; i < (int)'z'; i++)
+	{
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
+
+
