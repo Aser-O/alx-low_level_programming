35d16db70c1a83acb2b1eafb056775b9d4a2264b
Aser-O <Aserosama1@gmail.com>
2023-08-15 11:51:49 -0700

    3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _islower - checks upper or lower
+ * @a: the value being checked
+ *
+ * Return: 1 for success 0 for error
+ */
+int _islower(int a)
+{
+	return (a >= 97 ? 1 : 0);
+}
+
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,9 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char);
+int _putchar(char c);
 void print_alphabet();
 void print_alphabet_x10();
+int _islower(int c);
 
 #endif
