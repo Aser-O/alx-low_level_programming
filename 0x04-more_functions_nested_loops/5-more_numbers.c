96d330c13f46c2db4a03312e98b5ca349aba9703
Aser-O <Aserosama1@gmail.com>
2023-08-17 16:03:27 -0700

    f

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,17 +7,19 @@ void more_numbers(void)
 {
 	int i, j;
 
-	for (j = 0; j < 11; j++)
+	for (j = 0; j < 10; j++)
 	{
-		int v = 0;
-		if (i >= 10)
-		{
-			_putchar('1');
-			v = 10;
-		}
-		else if (j != 10){
-			for (i = 0; i <= 15; i++)
-			{
+
+		if (j != 10){
+			for (i = 0; i <= 14; i++)
+			{	
+				int v = 0;
+			
+				if (i >= 10)
+				{
+					_putchar('1');
+					v = 10;
+				}
 				_putchar(i + '0' - v);
 			}
 		}
