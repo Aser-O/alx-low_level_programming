639b5e54f6393a56b656ddc993af3feb61d75415
Aser-O <Aserosama1@gmail.com>
2023-08-17 14:46:03 -0700

    00

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * _isupper - checks if char is uppercase or lowercase
+ *
+ * @a: char to check
+ * Return: 1 if upper 0 is lower
+ */
+
 int _isupper(int a)
 {
 	return (a <= 90 && a >= 65);
