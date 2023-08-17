81c8a01ffa4c7a4267346df59031e1562b8b45c5
Aser-O <Aserosama1@gmail.com>
2023-08-17 15:29:29 -0700

    23

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - checks if char is uppercase or lowercase
+ *
+ * @a: char to check
+ * Return: 1 if upper 0 is lower
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - description
+ *
+ * Return: description
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar((char)i);
+	}
+	_putchar('\n');
+}
