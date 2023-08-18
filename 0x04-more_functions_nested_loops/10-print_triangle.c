503b6bde57674e529fe3533fda8bed7e4bae014e
Aser-O <Aserosama1@gmail.com>
2023-08-17 18:54:01 -0700

    tri

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @n: num of layers
+ * Return: void
+ */
+void print_triangle(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= n; i++)
+		{
+			for (j = i; j < n; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
