40f08afc8dc757c45d8c278df9f4f70784fbc15a
Aser-O <Aserosama1@gmail.com>
2023-08-21 12:59:59 -0700

    4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints string reversed
+ * @s: arr of char
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i = 0;
+
+	do {
+		if (s[i] != '\0')
+			i++;
+		else
+			break;
+	} while (1);
+
+	for (; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
