9ccd04d9b864c39f59342f9a546d847490ec5f9e
Aser-O <Aserosama1@gmail.com>
2023-08-24 16:00:59 -0700

    6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,19 +1,34 @@
 #include "main.h"
 
 /**
-* cap_string - makes a string upper case
-* @c: the word
-* Return: void
-*/
-char *cap_string(char *c)
+ * cap_string - makes a string upper case
+ * @a: the word
+ * Return: void
+ */
+char *cap_string(char *a)
 {
 	int i = 0;
+	int sep = 0;
 
 	do {
-		if (c[i] == '\0')
-			return (c);
-		if (c[i] >= 'a' && c[i] <= 'z')
-			c[i] += 'A' - 'a';
+		if (a[i] == '\0')
+			return (a);
+
+		if (a[i] == ' ' || a[i] == '\t' || a[i] == '\n' || a[i] == ',' ||
+				a[i] == ';' || a[i] == '.' || a[i] == '!' ||
+				a[i] == '?' || a[i] == '"' || a[i] == '(' ||
+				a[i] == ')' || a[i] == '{' || a[i] == '}')
+		{
+			sep = 1;
+			i++;
+			continue;
+		}
+
+		if ((a[i] >= 'a' && a[i] <= 'z') && sep == 1)
+		{
+			a[i] += 'A' - 'a';
+			sep = 0;
+		}
 		i++;
 	} while (1);
 }
