1fbe861ce6e0a50345dd62ee5b3fa11078db6a8c
Aser-O <Aserosama1@gmail.com>
2023-08-24 13:43:20 -0700

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,14 +4,15 @@
 * reverse_array - reverses an array
 * @a: array
 * @n: num of elems
-* Return: voif
+* Return: void
 */
 void reverse_array(int *a, int n)
 {
-	int i, tmp;
+	int i, tmp, end;
 
+	end = n / 2;
 	n--;
-	for (i = 0; i <= n / 2; i++)
+	for (i = 0; i <= end; i++)
 	{
 		tmp = a[i];
 		a[i] = a[n - i];
